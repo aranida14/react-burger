832746be7531d7881d9e8c611b4c7c027a4b8728
aranida14 <dinara1602@gmail.com>
2024-04-02 16:16:15 +0300

    header improvement

--- src/components/app-header/app-header.js
+++ src/components/app-header/app-header.js
@@ -7,17 +7,23 @@ function AppHeader() {
       <nav className={ `${styles.nav} pt-4 pb-4` }>
         <ul className={ `${styles.menu} ` }>
           <li className={ `${styles.burgerConstructor} m-5 mr-7` }>
-            <BurgerIcon type="primary" />
-            <a className="text text_type_main-default ml-2" href="#">Конструктор</a>
+            <a href="#">
+              <BurgerIcon type="primary" />
+              <span className="text text_type_main-default ml-2">Конструктор</span>
+            </a>
           </li>
           <li className={ `${styles.orders} m-5`}>
-            <ListIcon type="secondary" />
-            <a className="text text_type_main-default text_color_inactive ml-2" href="#">Лента заказов</a>
+            <a href="#">
+              <ListIcon type="secondary" />
+              <span className="text text_type_main-default text_color_inactive ml-2">Лента заказов</span>
+            </a>
           </li>
           <li className={ styles.logo }><Logo /></li>
           <li className={ `${styles.profile} m-5` }>
-            <ProfileIcon type="secondary" />
-            <a className="text text_type_main-default text_color_inactive ml-2" href="#">Личный кабинет</a>
+            <a href="#">
+              <ProfileIcon type="secondary" />
+              <span className="text text_type_main-default text_color_inactive ml-2">Личный кабинет</span>
+            </a>
           </li>
         </ul>
       </nav>
