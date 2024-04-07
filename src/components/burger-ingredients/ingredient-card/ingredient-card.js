50b9ae99269cc5383873501a8d5d8869763af744
aranida14 <dinara1602@gmail.com>
2024-04-07 05:03:26 +0300

    add ingredient counter

--- src/components/burger-ingredients/ingredient-card/ingredient-card.js
+++ src/components/burger-ingredients/ingredient-card/ingredient-card.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import { useMemo } from 'react';
 import styles from './ingredient-card.module.css';
 import { Counter, CurrencyIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 import Modal from '../../modal/modal';
@@ -9,8 +9,17 @@ import { showIngredient, closeIngredient } from '../../../services/current-ingre
 
 const IngredientCard = ({ data }) => {
   const {_id, image, name, price} = data;
-  
-  const currentIngredient = useSelector((store) => store.currentIngredient.data);
+  const { bun, ingredients: constructorIngredients } = useSelector((state) => state.burgerConstructor);
+
+  const count = useMemo(() => {
+    if (bun && bun._id === _id) {
+      return 2;
+    }
+   return constructorIngredients
+    .reduce((acc, item) => item._id === _id ? acc + 1 : acc, 0);
+  }, [bun, constructorIngredients]);
+
+  const currentIngredient = useSelector((state) => state.currentIngredient.data);
   const dispatch = useDispatch();
 
   const handleClick = () => {
@@ -24,7 +33,7 @@ const IngredientCard = ({ data }) => {
   return (
       <>
         <li className={ styles.card + ' ml-4 mr-2 mb-10'} onClick={handleClick}>
-          { _id === '60666c42cc7b410027a1a9b1' && <Counter count={2} size="default" extraClass="m-1" /> }
+          { count > 0 && <Counter count={count} size="default" extraClass="m-1" /> }
           <img src={image} alt={name} className='ml-4 mr-4'/>
           <div className={ `${styles.priceContainer} mt-1 mb-1`}>
             <span className={ `text text_type_digits-default ${styles.price}`}>{price}</span>
