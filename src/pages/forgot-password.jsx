67d4252c4cc0f46dab2bc1309c8d6cb222b3088c
aranida14 <dinara1602@gmail.com>
2024-04-26 18:21:10 +0300

    reset and forgot password api requests

--- src/pages/forgot-password.jsx
+++ src/pages/forgot-password.jsx
@@ -1,30 +1,59 @@
 import styles from './login.module.css';
 import AppHeader from '../components/app-header/app-header';
 import { Button, EmailInput } from '@ya.praktikum/react-developer-burger-ui-components';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { request } from '../utils/utils';
+import { useState } from 'react';
 
 export const ForgotPasswordPage = () => {
-  const onClick = () => {};
+  const navigate = useNavigate();
+  const [email, setEmail] = useState('');
+  const onChange = (e) => {
+    setEmail(e.target.value);
+  }
+
+  const resetPassword = (e) => {
+    e.preventDefault();
+    request('/password-reset', {
+      method: "POST",
+      body: JSON.stringify({ email: email }),
+      headers: {
+          "Content-Type": "application/json; charset=UTF-8"
+      },
+    }).then((data) => {
+      if (data.success) {
+        navigate('/reset-password');
+      } else {
+        console.error('Ошибка');
+      }
+      
+    }).catch((e) => console.error(e));
+  };
   return (
     <div className='mainContainer'>
       <AppHeader />
       <div className={styles.container}>
         <h2 className='text text_type_main-medium'>Восстановление пароля</h2>
-        <EmailInput
-          name={'email'}
-          placeholder='Укажите e-mail'
-          isIcon={false}
-          extraClass='mt-6'
-        />
-        <Button
-          htmlType="submit"
-          type="primary"
-          size="medium"
-          extraClass='mb-20 mt-6'
-          onClick={onClick}
-        >
-          Восстановить
-        </Button>
+        <form onSubmit={resetPassword}>
+          <EmailInput
+            name={'email'}
+            value={email}
+            placeholder='Укажите e-mail'
+            isIcon={false}
+            onChange={onChange}
+            extraClass='mt-6'
+            required
+          />
+          <Button
+            htmlType="submit"
+            type="primary"
+            size="medium"
+            extraClass='mb-20 mt-6'
+          >
+            Восстановить
+          </Button>
+        </form>
+        
         <div className={styles.linkContainer}>
           <p className='text text_type_main-default text_color_inactive'>Вспомнили пароль?</p>
           <Link to='/login'>
--- src/pages/reset-password.jsx
+++ src/pages/reset-password.jsx
@@ -1,37 +1,83 @@
 import styles from './login.module.css';
 import AppHeader from '../components/app-header/app-header';
 import { Button, PasswordInput, Input } from '@ya.praktikum/react-developer-burger-ui-components';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import { request } from '../utils/utils';
 
 export const ResetPasswordPage = () => {
-  const onClick = () => {};
+  const [password, setPassword] = useState('');
+  const [checkCode, setCheckCode] = useState('');
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  const navigate = useNavigate();
+
+  const changeCheckCodeInput = (e) => {
+    setCheckCode(e.target.value);
+    setErrorMsg(null);
+  }
+
+  const changePasswordInput = (e) => {
+    setPassword(e.target.value);
+  }
+
+  const resetPassword = (e) => {
+    e.preventDefault();
+    request('/password-reset/reset', {
+      method: "POST",
+      body: JSON.stringify({ password: password, token: checkCode }),
+      headers: {
+          "Content-Type": "application/json; charset=UTF-8"
+      },
+    }).then((data) => {
+      if (data.success) {
+        setErrorMsg(null);
+        navigate('/', { replace: true });
+      } else {
+        setErrorMsg('Некорректно введён код из письма');
+      }
+      
+    }).catch((e) => {
+      console.error(e);
+      setErrorMsg('Некорректно введён код из письма');
+    });  
+  };
   return (
     <div className='mainContainer'>
       <AppHeader />
       <div className={styles.container}>
         <h2 className='text text_type_main-medium'>Восстановление пароля</h2>
-        <PasswordInput
-          name={'password'}
-          placeholder='Введите новый пароль'
-          extraClass="mt-6"
-        />
-        <Input
-          type={'text'}
-          placeholder={'Введите код из письма'}
-          onChange={() => {}}
-          name={'code'}
-          size={'default'}
-          extraClass='mt-6'
-        />
-        <Button
-          htmlType="submit"
-          type="primary"
-          size="medium"
-          extraClass='mb-20 mt-6'
-          onClick={onClick}
-        >
-          Сохранить
-        </Button>
+        { errorMsg
+          ? <p className={`${styles.error} text text_type_main-default`}>{errorMsg}</p>
+          : null }
+        <form onSubmit={resetPassword}>
+          <PasswordInput
+            name={'password'}
+            placeholder='Введите новый пароль'
+            onChange={changePasswordInput}
+            value={password}
+            extraClass="mt-6"
+            required
+          />
+          <Input
+            type={'text'}
+            placeholder={'Введите код из письма'}
+            onChange={changeCheckCodeInput}
+            value={checkCode}
+            name={'checkCode'}
+            size={'default'}
+            extraClass='mt-6'
+            required
+          />
+          <Button
+            htmlType="submit"
+            type="primary"
+            size="medium"
+            extraClass='mb-20 mt-6'
+          >
+            Сохранить
+          </Button>
+        </form>
         <div className={styles.linkContainer}>
           <p className='text text_type_main-default text_color_inactive'>Вспомнили пароль?</p>
           <Link to='/login'>
