6a96a6c8c0ed5afc4874b9a64de4c71853a3a7c6
aranida14 <dinara1602@gmail.com>
2024-05-04 22:29:43 +0300

    remove current ingredient from redux

--- src/services/store.js
+++ src/services/store.js
@@ -1,7 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit'
 import ingredientsReducer from './ingredients-slice';
 import burgerConstructorReducer from './burger-constructor-slice';
-import currentIngredientReducer from './current-ingredient-slice';
 import orderReducer from './order-slice';
 import userReducer from './user-slice';
 
@@ -9,7 +8,6 @@ export const store = configureStore({
   reducer: {
     ingredients: ingredientsReducer,
     burgerConstructor: burgerConstructorReducer,
-    currentIngredient: currentIngredientReducer,
     order: orderReducer,
     user: userReducer,
   }
